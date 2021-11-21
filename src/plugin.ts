75a89b8d4df84d34b5a6361b871516ffc279c19d
larsmarginet <lars.marginet@student.howest.be>
2021-11-21 11:24:37 +0100

    feat: download file

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,13 +1,26 @@
-function logHelloWorld(payload: any): void {
-  console.log(payload.msg);
-}
+const generateFile = (payload: any): void => {
+  postMessages({
+    type: 'downlooad-file',
+    payload: [
+      { var: '--color-primary', value: '#ffffff' },
+      { var: '--color-secondary', value: '#000000' },
+    ],
+  });
+};
 
-figma.ui.onmessage = function ({ type, payload }): void {
+// send messgages to ui.js
+const postMessages = ({ type, payload }: any): void => {
+  figma.ui.postMessage({ type, payload });
+};
+
+// listen for messages from ui.js
+figma.ui.onmessage = ({ type, payload }): void => {
   switch (type) {
-    case 'hello-world':
-      logHelloWorld(payload);
+    case 'generate-file':
+      generateFile(payload);
       break;
   }
 };
 
+// create the ui window
 figma.showUI(__html__, { width: 300, height: 200 });
--- src/ui.js
+++ src/ui.js
@@ -1,21 +1,50 @@
 import './ui.css';
+import { saveAs } from 'file-saver';
 
+const downloadFile = (payload) => {
+  let variables = '';
+  payload.map((item) => (variables += `${item.var}: ${item.value};`));
+
+  const content = `:root {${variables}}`;
+  const filename = 'colors.scss';
+  const blob = new Blob([content], {
+    type: 'text/plain;charset=utf-8',
+  });
+
+  saveAs(blob, filename);
+};
+
+// send messages to plugin.ts
 const postMessage = (type, payload) => {
   parent.postMessage({ pluginMessage: { type, payload } }, '*');
 };
 
-const buttonListeners = () => {
-  document.addEventListener('click', function (event) {
+// listen for messages from plugin.ts
+const messageListener = () => {
+  window.onmessage = (e) => {
+    const pluginMessage = e.data.pluginMessage;
+    const { type, payload } = pluginMessage;
+
+    switch (type) {
+      case 'downlooad-file':
+        downloadFile(payload);
+        break;
+    }
+  };
+};
+
+// listen to click events of user
+const clickListeners = () => {
+  document.addEventListener('click', (event) => {
     const target = event.target;
 
     switch (target.id) {
-      case 'helloWorld':
-        postMessage('hello-world', {
-          msg: 'hello world 👋',
-        });
+      case 'generateFile':
+        postMessage('generate-file');
         break;
     }
   });
 };
 
-buttonListeners();
+clickListeners();
+messageListener();
